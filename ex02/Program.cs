16babe84bf0524bbe6a30480a632fc27b8527dd3
OlegLegezo <oleglegezo@yandex.ru>
2022-06-03 09:55:08 +0300

    матрица

---
+++ ex02/Program.cs
@@ -0,0 +1,10 @@
+﻿//мтарица чисел
+int[,] matrix = new int[3, 4];
+for (int i = 0; i < matrix.GetLength(0); i++)//matrix.GetLength(0) вместо написания руками 3
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)//matrix.GetLength(0) вместо написания руками 4
+    {
+        Console.Write($"{matrix[i, j]} ");
+    }
+    Console.WriteLine();
+}
