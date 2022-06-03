109ad307c3eead397096f8941ebe6e6cd0a208b7
OlegLegezo <oleglegezo@yandex.ru>
2022-06-03 12:27:53 +0300

    рекурсия числа Фибонначи

---
+++ ex06/Program.cs
@@ -0,0 +1,15 @@
+﻿// числа Фибоначчи
+//f(n)=f(n-1)+f(n-2)
+
+int Fibonacci (int n)
+{
+    if(n==1||n==2)// "||" - знак или
+    return 1;
+    else 
+    return Fibonacci(n-1)+Fibonacci(n-2);
+}
+
+for (int i = 1; i < 10; i++)
+{
+    Console.WriteLine($"f({i})={Fibonacci(i)}");
+}
